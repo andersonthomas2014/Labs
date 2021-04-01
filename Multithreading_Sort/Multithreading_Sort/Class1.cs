072134450ae63a4fcb31be5d1a67dc90d629b357
andersonthomas2014 <anderson.thomas2014@mail.ru>
2021-04-01 12:44:40 +0500

    Do something

---
+++ Multithreading_Sort/Multithreading_Sort/Class1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Multithreading_Sort
+{
+    class MThread_Sorter
+    {
+        public MThread_Sorter()
+        {
+
+            
+        }
+
+        
+        private object locker = new object();
+        
+        
+
+        private void Swap(ref int e1, ref int e2)
+        {
+            var temp = e1;
+            e1 = e2;
+            e2 = temp;
+        }
+        public void Bubble_Sort(int[] array, Semaphore starter,Semaphore output, bool locked)
+        {
+            starter.Release();
+            
+            int len = array.Length;
+            for (int i = 1; i < len; i++)
+            {
+                for (int j = 0; j < len - i; j++)
+                {
+                    if (locked)
+                    {
+
+                        lock (locker)
+                        {
+                            if (array[j] > array[j + 1])
+                            {
+                                Swap(ref array[j], ref array[j + 1]);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        if (array[j] > array[j + 1])
+                        {
+                            
+                            Swap(ref array[j], ref array[j + 1]);
+                        }
+                    }
+                }
+            }
+            output.Release();
+            return;
+            
+        }
+        public void Selection_Max_Sort(int[] array, Semaphore starter, Semaphore output, bool locked)
+        {
+            starter.WaitOne();
+
+            int max, temp;
+            int length = array.Length;
+
+            for (int i = 0; i < length - 1; i++)
+            {
+                max = i;
+                lock (locker)
+                {
+                    for (int j = 0; j < length - i; j++)
+                    {
+
+
+                        if (array[j] > array[max])
+                        {
+
+                            max = j;
+                        }
+
+
+                    }
+
+                    Thread.Sleep(2);
+                    if (max != i)
+                    {
+                        temp = array[length - i - 1];
+                        array[length - i - 1] = array[max];
+                        array[max] = temp;
+                    }
+                }
+            }
+
+            output.Release();
+            return;
+        }
+    }
+}
---
+++ Multithreading_Sort/Multithreading_Sort/MainWindow.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+
+namespace Multithreading_Sort
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        const int min = 0, max = 100;
+
+        public int[] array;
+         
+        
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Sort.Click += Sort_Click;
+            Generation.Click += Generation_Click;
+
+        }
+
+      
+
+        private void Sort_Click(object sender, RoutedEventArgs e)
+        {
+            SortedArray.Text = null;
+
+            bool locking = (bool) Locker.IsChecked;
+            Semaphore starter = new Semaphore(0, 2);
+            Semaphore bubble_output, selection_max_output;
+            bubble_output = new Semaphore(0, 1);
+            selection_max_output = new Semaphore(0, 1);
+
+            MThread_Sorter Sorter = new MThread_Sorter();
+            Thread Bubble_Sort_Thread = new Thread(() => Sorter.Bubble_Sort(array, starter, bubble_output, locking));
+            Thread Max_Sort_Thread = new Thread(() => Sorter.Selection_Max_Sort(array, starter, selection_max_output, locking));
+            
+            Max_Sort_Thread.Start();
+            Bubble_Sort_Thread.Start();
+
+
+            //starter.Release(2);
+            bubble_output.WaitOne();
+            selection_max_output.WaitOne();
+           
+
+            
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                SortedArray.Text += array[i] + " ";
+            }
+
+        }
+
+       
+
+        private void Generation_Click(object sender, RoutedEventArgs e)
+        {
+            GeneratedArray.Text = null;
+
+            Random random = new Random();
+            array = new int[Convert.ToInt32(Number.Text)];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(min, max);
+                GeneratedArray.Text += array[i] + " ";
+            }
+        }
+
+        
+        
+       
+
+       
+    }
+}
